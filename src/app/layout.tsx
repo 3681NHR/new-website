d9c8aa10a0644c4da65bbdb0cd1dd8c892056d4b
Gavan <126712894+StubblyCamera52@users.noreply.github.com>
2024-12-11 10:25:03 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
           <NavbarItem title="Join" href="/join"></NavbarItem>
           <AppearanceToggle />
         </Navbar>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&si=-fEaxswGapueRfcQ&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         {children}
       </body>
     </html>
