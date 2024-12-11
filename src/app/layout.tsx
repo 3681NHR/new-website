47dac82f0cae42712fdb08a280054225d03510fd
Gavan <126712894+StubblyCamera52@users.noreply.github.com>
2024-12-11 18:36:22 +0000

    fix lint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,16 @@ export default function RootLayout({
           <NavbarItem title="Join" href="/join"></NavbarItem>
           <AppearanceToggle />
         </Navbar>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&si=-fEaxswGapueRfcQ&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&si=-fEaxswGapueRfcQ&amp;controls=0"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
         {children}
       </body>
     </html>
