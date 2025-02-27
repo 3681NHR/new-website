da076cb7ccfea1f71a83207c8935f6b8d6e113dc
Gavan <gavanbess@gmail.com>
2025-02-26 21:24:46 -0800

    SAM

--- src/components/other/heroImage.tsx
+++ src/components/other/heroImage.tsx
@@ -1,17 +1,41 @@
+'use client';
+
 import { Image as ChakraImage } from '@chakra-ui/react';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 
 export default function HeroImage() {
+  const [sam, setSam] = useState(false);
+
+  useEffect(() => {
+    setSam(Math.floor(Math.random() * 100) === 33);
+  }, []);
+
   return (
-    <ChakraImage zIndex={0} asChild>
-      <Image
-        src="/logo.svg"
-        alt="Cover Image"
-        width={1000}
-        height={1000}
-        //fill
-        priority
-      />
-    </ChakraImage>
+    <>
+      {sam ? (
+        <ChakraImage zIndex={0} asChild>
+          <Image
+            src="/sam.png"
+            alt="Cover Image"
+            width={1000}
+            height={1000}
+            //fill
+            priority
+          />
+        </ChakraImage>
+      ) : (
+        <ChakraImage zIndex={0} asChild>
+          <Image
+            src="/logo.svg"
+            alt="Cover Image"
+            width={1000}
+            height={1000}
+            //fill
+            priority
+          />
+        </ChakraImage>
+      )}
+    </>
   );
 }
